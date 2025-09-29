71ba334495e1f3488a20c5d689ff23b69603ff03
0fekree0 <70841845+FeKree@users.noreply.github.com>
2025-09-29 17:23:08 +0300

    1

--- LR1/1.cs
+++ LR1/1.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ConsoleApp1
 {
     internal class Program
@@ -19,7 +25,7 @@ namespace ConsoleApp1
             }
             Console.WriteLine("Введите f(x) от 1 до 3");
             c = Convert.ToInt32(Console.ReadLine());
-            if(c > 3 || c < 1)
+            if (c > 3 || c < 1)
             {
                 Console.WriteLine("Неверное число");
             }
@@ -27,11 +33,11 @@ namespace ConsoleApp1
             {
                 f = 2 * x;
             }
-            else if(c == 2)
+            else if (c == 2)
             {
                 f = x * x;
             }
-            else if(c == 3)
+            else if (c == 3)
             {
                 f = x / 3;
             }
@@ -40,6 +46,10 @@ namespace ConsoleApp1
             y = b * Math.Cos(a * f) + Math.Sin(x / 5) + a * Math.Exp(x);
             Console.WriteLine("a = " + a + " b = " + b + " f(x) = " + f + " x = " + x);
             Console.WriteLine("y = " + y);
+            Console.WriteLine(string.Format("a = {0} b = {1} f(x) = {2} x = {3}", a, b, f, x));
+            Console.WriteLine(string.Format("y = {0}", y));
+            Console.WriteLine("a = {0} b = {1} f(x) = {2} x = {3}", a, b, f, x);
+            Console.WriteLine("y = {0}", y);
             Console.ReadKey();
         }
     }
